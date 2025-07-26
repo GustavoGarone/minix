b11fdaf57cc37e2cafae9dd04c6a08e9f3a4548c
Minze <gustavogarone@protonmail.com>
2025-07-26 15:26:26 -0300

    hyprland cachix

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -21,6 +21,12 @@
     GDK_BACKEND = "wayland";
     MOZ_ENABLE_WAYLAND = "1";
   };
+  # Cachix to not build Hyprland depedencies
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
   # Hyprland
   programs.hyprland = {
     enable = true;
