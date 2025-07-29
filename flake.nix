e335667bfe01a3fed8f044833972786cf70665a8
Minze <gustavogarone@protonmail.com>
2025-07-29 13:11:47 -0300

    changing Hyprland pkg

--- flake.nix
+++ flake.nix
@@ -56,10 +56,10 @@
             {
               wayland.windowManager.hyprland = {
                 enable = true;
-                # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-                # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-                package = null;
-                portalPackage = null;
+                package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+                portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+                # package = null;
+                # portalPackage = null;
               };
             }
           ];
