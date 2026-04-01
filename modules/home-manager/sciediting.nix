7d1301a321f4a4d6634486c239a50a28c79b235b
Gustavo Garone <gustavogarone@protonmail.com>
2026-04-01 17:10:46 -0300

    feat: delegates sciediting to project specific flakes

--- modules/home-manager/sciediting.nix
+++ modules/home-manager/sciediting.nix
@@ -1,17 +1,8 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    rPackages.languageserver
-    rPackages.knitr
-    rPackages.rmarkdown
-    rPackages.quarto
-    rPackages.air
     marksman
     librsvg # Convert svgs
-    air-formatter
-    quarto
     zotero
-    (texlive.withPackages
-      (ps: [ps.scheme-small ps.luatex]))
     tex-fmt # Latex formatter in rust
     poppler-utils # For converting pdf to svg (pgfxplots)
   ];
