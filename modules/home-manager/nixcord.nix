46b49ba5ab4e6162de26efd369079f177f2d0fe6
Gustavo Garone <gustavogarone@protonmail.com>
2026-05-03 21:08:40 -0300

    discord: drops vesktop for streaming compatibility

--- modules/home-manager/nixcord.nix
+++ modules/home-manager/nixcord.nix
@@ -1,8 +1,11 @@
 {
   programs.nixcord = {
     enable = true; # enable Nixcord. Also installs discord package
-    discord.enable = false;
-    vesktop.enable = true;
+    discord = {
+      enable = true;
+      vencord.enable = true;
+    };
+    vesktop.enable = false;
     config = {
       plugins = {
         fakeNitro.enable = true;
