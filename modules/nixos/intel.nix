34533136ac9b1e423999f2776fe69490fbd9c566
Minze <gustavogarone@protonmail.com>
2026-05-26 23:27:25 -0300

    Intel: General driver optimisations

--- modules/nixos/intel.nix
+++ modules/nixos/intel.nix
@@ -3,8 +3,14 @@
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
+      intel-media-driver
       vpl-gpu-rt
     ];
   };
   boot.kernelParams = ["i915.force_probe=46b3"];
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+  hardware.enableRedistributableFirmware = true;
+  services.xserver.videoDrivers = ["modesetting"];
 }
