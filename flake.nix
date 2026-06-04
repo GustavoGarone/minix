871faa8508bdba4161a1f83b7522a36e38a15224
Gustavo Garone <gustavogarone@protonmail.com>
2026-06-03 22:34:17 -0300

    Update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   description = "Minix config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     zed.url = "github:zed-industries/zed";
     stylix = {
-      url = "github:danth/stylix/release-25.11";
+      url = "github:danth/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     niri = {
@@ -59,14 +59,14 @@
 
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       specialArgs = {
-        inherit inputs system outputs;
+        inherit inputs outputs;
       };
       modules = [
         ./desktop/configuration.nix
         stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager
         {
-          home-manager.extraSpecialArgs = {inherit inputs system outputs;};
+          home-manager.extraSpecialArgs = {inherit inputs outputs;};
           home-manager.users.minze = import ./desktop/home.nix;
           home-manager.backupFileExtension = "bkphm";
           home-manager.sharedModules = [
@@ -79,14 +79,14 @@
     };
 
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs system outputs;};
+      specialArgs = {inherit inputs outputs;};
       modules = [
         ./laptop/configuration.nix
         stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager
         auto-cpufreq.nixosModules.default
         {
-          home-manager.extraSpecialArgs = {inherit inputs system outputs;};
+          home-manager.extraSpecialArgs = {inherit inputs outputs;};
           home-manager.users.minze = import ./laptop/home.nix;
           home-manager.backupFileExtension = "bkphm";
           home-manager.sharedModules = [
