bb84fc603615baa242b36741639b23fddd73c9eb
Minze <gustavogarone@protonmail.com>
2026-07-13 22:17:08 -0300

    optims: adds automatic garbage collection to home manager
    optims: adds automatic store optimisation to home manager

--- modules/aspects/optims.nix
+++ modules/aspects/optims.nix
@@ -1,13 +1,16 @@
 {
-  den.aspects.optims = {
-    nixos = {
-      nix.settings.auto-optimise-store = true;
-      nix.gc = {
+  den.aspects.optims = let
+    nix = {
+      gc = {
         automatic = true;
         dates = "15d";
         options = "--delete-older-than 15d";
       };
-
+      settings.auto-optimise-store = true;
+    };
+  in {
+    nixos = {
+      inherit nix;
       zramSwap = {
         enable = true;
         priority = 100;
@@ -17,6 +20,9 @@
       services.thermald.enable = true;
 
       fileSystems."/".options = ["noatime"];
+    };
+    home-manager = {
+      inherit nix;
     };
   };
 }
