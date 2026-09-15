aee39ea848e60e5380b267b899cbd8bc332cb82c
Gustavo Garone <gustavogarone@protonmail.com>
2026-09-15 15:34:03 -0300

    fix: upstream gsr issue

--- modules/aspects/apps/gsr.nix
+++ modules/aspects/apps/gsr.nix
@@ -5,6 +5,16 @@
       package = inputs.gsr-ui-nix.packages.${pkgs.stdenv.hostPlatform.system}.gpu-screen-recorder;
       enable = true;
       ui.enable = true;
+      # Work around upstream passing fetchSubmodules to builtins.fetchGit.
+      ui.notificationPackage =
+        inputs.gsr-ui-nix.packages.${pkgs.stdenv.hostPlatform.system}.gpu-screen-recorder-notification.overrideAttrs
+        (_: {
+          src = builtins.fetchGit {
+            url = "https://repo.dec05eba.com/gpu-screen-recorder-notification";
+            rev = "54bc6c8f139a5ff4025a4585d01c9e3f23894b72";
+            submodules = true;
+          };
+        });
     };
   };
 }
